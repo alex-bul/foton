2628605f850f54a7a9df3fae040bb627b1c60bdf
Alexander Bulbenkov <bualal2005@gmail.com>
2021-04-23 01:44:40 +0200

    finish portrait processing

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 HOST = '127.0.0.2'
 PORT = 8080
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 STATIC_FOLDER = 'static'
 USER_PHOTOS_FOLDER = 'user_photos'
 UPLOAD_FOLDER = f'{STATIC_FOLDER}/{USER_PHOTOS_FOLDER}'
@@ -12,7 +11,6 @@ catalog = [
         'description': "Создает уникальный портрет с вашим лицом. <br><b>Ван Гог</b> обзавидуется",
         'icon': "img/portrait.svg",
         'form_value': ['Загрузите фото человека'],
-        'url': 'fddd'
     },
     {
         'title': "Комикс",
@@ -20,10 +18,10 @@ catalog = [
         'icon': "img/comics.svg",
         'form_value': ['Загрузите фото человека']
     },
-    {
-        'title': "Замена лиц",
-        'description': "Алгоритм берет лицо с одного человека и накладывает на другого",
-        'icon': "img/face.svg",
-        'form_value': []
-    },
+    # {
+    #     'title': "Замена лиц",
+    #     'description': "Алгоритм берет лицо с одного человека и накладывает на другого",
+    #     'icon': "img/face.svg",
+    #     'form_value': ['Загрузите фото человека (у него берется лицо)', 'Загрузите фото человека (ему меняется лицо)'],
+    # },
 ]
--- data/catalog_form.py
+++ data/catalog_form.py
@@ -1,9 +1,6 @@
-# from flask_uploads import UploadSet, IMAGES
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-
-# images = UploadSet('images', IMAGES)
 
 
 def get_catalog_form(form_value):
